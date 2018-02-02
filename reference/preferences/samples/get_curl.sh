1b3e0cb39234d68812dff08a70c2c29314651c89
Joel Ibaceta <joel.ibaceta@mercadolibre.com>
2018-02-02 17:10:41 -0300

    adding samples for preferences

---
+++ reference/preferences/samples/get_curl.sh
@@ -0,0 +1,2 @@
+curl -X GET \
+  'https://api.mercadopago.com/checkout/preferences/:id?access_token=ACCESS_TOKEN_ENV'
---
+++ reference/preferences/samples/post_curl.sh
@@ -0,0 +1,13 @@
+curl -X POST \
+  'https://api.mercadopago.com/checkout/preferences?access_token=ACCESS_TOKEN_ENV' \
+  -d '{
+        "items": [
+            {
+            "title": "Dummy Item",
+            "description": "Multicolor Item",
+            "quantity": 1,
+            "currency_id": "ARS",
+            "unit_price": 10.0
+            }
+        ]
+    }'
