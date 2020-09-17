2d3487daa392fa68326579428731e101e25cbca3
Javier Capotondo <jcapotondo@gmail.com>
2020-09-17 17:39:19 -0300

    adding access token as a header in preferences' reference

--- reference/preferences/endpoints/_checkout_preferences_search/samples/get_curl.sh
+++ reference/preferences/endpoints/_checkout_preferences_search/samples/get_curl.sh
@@ -1,2 +1,3 @@
 curl -X GET \
- 'https://api.mercadopago.com/checkout/preferences?access_token=ACCESS_TOKEN_ENV&sponsor_id=&external_reference=&site_id=&marketplace=&date_created_from=&date_created_to=&last_updated_from=&last_updated_to=&limit=&offset=&order_status='
+ 'https://api.mercadopago.com/checkout/preferences&sponsor_id=&external_reference=&site_id=&marketplace=&date_created_from=&date_created_to=&last_updated_from=&last_updated_to=&limit=&offset=&order_status=' \
+ -H 'Authorization: Bearer ACCESS_TOKEN_ENV' \
