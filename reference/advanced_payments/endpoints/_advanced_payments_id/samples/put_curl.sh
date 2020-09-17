3d97b5e429912d5e60c628ce84dcac23d9680a59
Javier Capotondo <jcapotondo@gmail.com>
2020-09-17 18:36:11 -0300

    adding access token in header in advance payments

--- reference/advanced_payments/endpoints/_advanced_payments_id/samples/put_curl.sh
+++ reference/advanced_payments/endpoints/_advanced_payments_id/samples/put_curl.sh
@@ -1,3 +1,3 @@
-curl -X GET \
+curl -X PUT \
     'https://api.mercadopago.com/v1/advanced_payments/{id}' \
     -H 'Authorization: Bearer MKT_ACCESS_TOKEN'
---
+++ reference/advanced_payments/endpoints/_advanced_payments_id_disbursements_disbursement_id_refunds/samples/post_curl.sh
@@ -0,0 +1,3 @@
+curl -X POST \
+  'https://api.mercadopago.com/advanced_payments/{id}/disbursements/{disbursement_id}/refunds' \
+  -H 'Authorization: Bearer MKT_ACCESS_TOKEN'
