5efe1204503b868f756a51473456de8bd7e14ac4
Javier Capotondo <jcapotondo@gmail.com>
2020-09-20 01:06:15 -0300

    pr fixes

--- reference/advanced_payments/endpoints/_advanced_payments_id/samples/get_curl.sh
+++ reference/advanced_payments/endpoints/_advanced_payments_id/samples/get_curl.sh
@@ -1,3 +1,3 @@
 curl -X GET \
-    'https://api.mercadopago.com/v1/advanced_payments/{id}' \
+    'https://api.mercadopago.com/v1/advanced_payments/:ID' \
     -H 'Authorization: Bearer MKT_ACCESS_TOKEN'
