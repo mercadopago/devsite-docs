11326c8c52ccad156fb57ae87ce14870ab486cbb
Nicolás Salvanés <nicolas.salvanes@mercadolibre.com>
2020-09-25 17:11:09 -0300

    Se actualiza endpoint con las opciones de busqueda validas

--- reference/customers/endpoints/_customers_search/samples/get_curl.sh
+++ reference/customers/endpoints/_customers_search/samples/get_curl.sh
@@ -1,3 +1,3 @@
 curl -X GET \
-  'https://api.mercadopago.com/v1/customers/search' \
+  'https://api.mercadopago.com/v1/customers/search?email=test@test.com' \
   -H 'Authorization: Bearer ACCESS_TOKEN_ENV'
